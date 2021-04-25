4ee2abc2477cfb3a12c75211932fc0f3cf6fda6b
Amulya Dixit <amulyadixit07@gmail.com>
2021-04-26 01:21:05 +0530

    Databse models

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from website import create_app
 
-app=create_app()
+app = create_app()
 
 #only when we run this not we import
-if __name__ == '__main__':
-
-#this will run our flask application    
+if __name__ == "__main__":
+    #this will run our flask application 
     app.run(debug=True)
 
-#(debug==True) means when we change something in the code then our server automatically
-#  restart again 
+ #(debug==True) means when we change something in the code then our server automatically
+ #  restart again 
